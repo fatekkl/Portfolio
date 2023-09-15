bd17b651f8102a03c0c62bccb610b603694980d4
fatekkl <mathtml.1105@gmail.com>
2023-09-15 13:00:58 -0300

    adiciando target blank para os links

--- src/Components/Main/index.js
+++ src/Components/Main/index.js
@@ -9,14 +9,14 @@ const LayoutPrincipal = () => {
             <span>Oi, Eu sou o Matheus. 👋</span>
             <p>Busco uma oportunidade de estágio para aplicar minhas habilidades e conhecimentos em tecnologias como Node.js, React.js, JavaScript, HTML e CSS. Tenho experiência em desenvolvimento utilizando HTML/CSS/JavaScript para criar aplicações web interativas e funcionais.  Minha proficiência em JavaScript me permite implementar recursos avançados, enquanto minhas habilidades em HTML e CSS garantem  a criação de interfaces atraentes e responsivas. Estou ansioso para contribuir e aprender em um ambiente de trabalho real, aproveitando  meu conjunto de habilidades para auxiliar no desenvolvimento de soluções web inovadoras,visando adquirir mais conhecimento durante meu período de estágio.</p>
         
-        <div className="LinksBotao">
+        <div className="LinksBotao"  target="_blank">
             <a href="https://www.linkedin.com/in/matheus-de-oliveira-ba5074280/">
                 <img src="/assets/linkedin.png" alt="ImagemLinkedIn"/>LinkedIn
             </a>
-            <a  href="https://github.com/fatekkl">
+            <a  href="https://github.com/fatekkl"  target="_blank">
                 <img src="/assets/github.png" alt="ImagemGithub"/> Github
             </a>
-            <a href="https://www.instagram.com/matheuskdask/">
+            <a href="https://www.instagram.com/matheuskdask/"  target="_blank">
                 <img src="/assets/instagram.png" alt="ImagemInstagram"/>Instagram
             </a>
         </div>
