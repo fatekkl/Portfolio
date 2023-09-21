d8bac51abb5e54f5c5e001fcb57ab4d35d7b9b39
fatekkl <mathtml.1105@gmail.com>
2023-09-21 00:31:52 -0300

    arrumando import

---
+++ src/Components/ModoEscuro/index.js
@@ -0,0 +1,15 @@
+import './ModoEscuro.css'
+
+
+const ModoEscuro = () => {
+    return (
+        <div>
+        <input type='checkbox' id='checkinput'/>
+        <label for='checkinput'id='label'>
+            <img src='/assets/sol.png'></img>
+        </label>
+        </div>
+    )
+}
+
+export default ModoEscuro
