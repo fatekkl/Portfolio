8c46a2cd40acb89f623098066f0076f0542c7674
fatekkl <mathtml.1105@gmail.com>
2023-09-22 00:18:49 -0300

    adicionando codigo e atribuindo estilos do botao

--- src/Components/ModoEscuro/index.js
+++ src/Components/ModoEscuro/index.js
@@ -1,12 +1,33 @@
 import './ModoEscuro.css'
 
+const headerid = document.querySelector('#headerid')
+
+const layoutmain = document.querySelector('#layout-main')
+
+const textlayout = document.querySelector('#txtlayout')
+
+const sectionprojetos = document.querySelector('#section-projetos')
+
+const section2 = document.querySelector('#section-sobre')
+
+const botaodark = document.querySelector('#label')
+
 
 const ModoEscuro = () => {
+
+
+    const classhandle = () => {
+        headerid.classList.toggle('dark')
+        botaodark.classList.toggle('dark')
+        layoutmain.classList.toggle('dark')
+        textlayout.classList.toggle('dark')
+    }
+
     return (
-        <div>
-        <input type='checkbox' id='checkinput'/>
-        <label for='checkinput'id='label'>
-            <img src='/assets/sol.png'></img>
+        <div className='div-modoescuro'>
+        <input onChange={classhandle} className='inputclass' type='checkbox' id='checkinput'/>
+        <label className='labelclasse' htmlFor='checkinput'id='label'>
+        <div className="ball"></div>
         </label>
         </div>
     )
