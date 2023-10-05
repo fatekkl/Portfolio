12d55d4b56d2912cf3af52c0da67d2c2cddc8ea0
fatekkl <mathtml.1105@gmail.com>
2023-10-05 13:49:06 -0300

    mudança de texto

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -6,7 +6,7 @@ const LayoutHeader = () => {
     <header id='headerid' className="header">
         <nav className="Navegacao">
         <h2>Inicio</h2>
-        <h2>Sobre Mim</h2>
+        <h2>Sobre</h2>
         <h2>Projetos</h2>
         <h2>Tecnologias</h2>
         </nav>
