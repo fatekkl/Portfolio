e559f11dc82d7c887e4195651b1bfee25220c41d
fatekkl <mathtml.1105@gmail.com>
2023-10-05 09:51:53 -0300

    ajustando responsividade para 450px+ e adicionando boas praticas ao codigo

--- src/Components/Projetos/index.js
+++ src/Components/Projetos/index.js
@@ -5,9 +5,6 @@ function Projetos () {
     return (
         <section id='section-projetos' className='container-section'>
             <div className='container-projects'>
-
-
-
             </div>            
         </section>
     )
