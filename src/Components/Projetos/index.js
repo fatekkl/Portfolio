90b4f3c8054f696ef41fc9a36e37e02afc387778
fatekkl <mathtml.1105@gmail.com>
2023-10-06 01:53:51 -0300

    adicionando simbolos das techs a sessão de projetos

--- src/Components/Projetos/index.js
+++ src/Components/Projetos/index.js
@@ -2,8 +2,17 @@ import './Projetos.css'
 
 
 function Projetos () {
+    
     return (
         <section id='section-projetos' className='container-section'>
+            <div className='container-techs'>
+                <img alt='badge'  src='./assets/reactbadge.png'/>
+                <img alt='badge'  src='./assets/jsbadge.png'/>
+                <img alt='badge'  src='./assets/nodebadge.png'/>
+                <img alt='badge'  src='./assets/gitbadge.png'/>
+                <img alt='badge'  src='./assets/cssbadge.png'/>
+                <img alt='badge'  src='./assets/htmlbadge.png'/>
+            </div>
             <div className='container-projects'>
             </div>            
         </section>
