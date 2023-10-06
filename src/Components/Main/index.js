a4934cca23c1c2080d56ebc0ea2493c9505445ae
fatekkl <mathtml.1105@gmail.com>
2023-10-06 05:41:10 -0300

    atualizando links

--- src/Components/Main/index.js
+++ src/Components/Main/index.js
@@ -10,7 +10,7 @@ const LayoutPrincipal = () => {
             <p>Busco uma oportunidade de estágio para aplicar minhas habilidades e conhecimentos em tecnologias como Node.js, React.js, JavaScript, HTML e CSS. Tenho experiência em desenvolvimento utilizando HTML/CSS/JavaScript para criar aplicações web interativas e funcionais.  Minha proficiência em JavaScript me permite implementar recursos avançados, enquanto minhas habilidades em HTML e CSS garantem  a criação de interfaces atraentes e responsivas. Estou ansioso para contribuir e aprender em um ambiente de trabalho real, aproveitando  meu conjunto de habilidades para auxiliar no desenvolvimento de soluções web inovadoras,visando adquirir mais conhecimento durante meu período de estágio.</p>
         
         <div className="LinksBotao">
-            <a href="https://www.linkedin.com/in/matheus-de-oliveira-ba5074280/" target='_blank' rel='noopener noreferrer'>
+            <a href="https://www.linkedin.com/in/fatekkl/" target='_blank' rel='noopener noreferrer'>
                 <img id='linkedinimg' src="/assets/linkedin.png" alt="ImagemLinkedIn"/>LinkedIn⠀
             </a>
             <a  href="https://github.com/fatekkl"  target="_blank" rel='noopener noreferrer'>
