5f8b6205cb295140c7c254ce9c86359eaa3b0948
fatekkl <mathtml.1105@gmail.com>
2023-11-07 15:15:56 -0300

    corrigindo textos

--- src/Components/Main/index.js
+++ src/Components/Main/index.js
@@ -7,8 +7,7 @@ const LayoutPrincipal = () => {
         <section id='txtlayout' className="LayoutTextos">
             <h1 id='h1html'>Front-End<br/> <strong>Developer</strong>💧</h1>
             <span>Oi, eu sou o Matheus. 👋</span>
-            <p>Busco uma oportunidade de estágio para aplicar minhas habilidades e conhecimentos em tecnologias como Node.js, React.js, JavaScript, HTML e CSS. Tenho experiência em desenvolvimento utilizando HTML/CSS/JavaScript para criar aplicações web interativas e funcionais.  Minha proficiência em JavaScript me permite implementar recursos avançados, enquanto minhas habilidades em HTML e CSS garantem  a criação de interfaces atraentes e responsivas. Estou ansioso para contribuir e aprender em um ambiente de trabalho real, aproveitando  meu conjunto de habilidades para auxiliar no desenvolvimento de soluções web inovadoras,visando adquirir mais conhecimento durante meu período de estágio.</p>
-        
+            <p>Busco uma oportunidade de estágio para aplicar minhas habilidades e conhecimentos em tecnologias como Node.js, React.js, JavaScript, HTML e CSS. Tenho experiência em desenvolvimento utilizando HTML/CSS/JavaScript para criar aplicações web interativas e funcionais.  Minha proficiência em JavaScript me permite implementar recursos avançados, enquanto minhas habilidades em HTML e CSS garantem  a criação de interfaces atraentes e responsivas. Estou ansioso para contribuir e aprender em um ambiente de trabalho real, aproveitando  meu conjunto de habilidades para auxiliar no desenvolvimento de soluções web inovadoras, visando adquirir mais conhecimento durante meu período de estágio.</p>
         <div className="LinksBotao">
             <a href="https://www.linkedin.com/in/fatekkl/" target='_blank' rel='noopener noreferrer'>
                 <img id='linkedinimg' src="/assets/linkedin.png" alt="ImagemLinkedIn"/>LinkedIn⠀
@@ -21,7 +20,6 @@ const LayoutPrincipal = () => {
             </a>
         </div>
         </section>
-    {/* <img className="SetaIMG" src="./assets/seta.png" alt="seta"/> */}
     </main>
 
 
