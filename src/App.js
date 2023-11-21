3d5c2f131b321fc2f825e82f9580fbc891aff49b
fatekkl <mathtml.1105@gmail.comm>
2023-11-21 11:46:13 -0300

    arrumando imports

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import LayoutPrincipal from "./Components/Main";
 import LayoutHeader from "./Components/Header";
 import SobreMim from "./Components/SobreMim";
 import Projetos from "./Components/Projetos";
-import ModoEscuro from "./Components/ModoEscuro";
 
 function App() {
   return (
