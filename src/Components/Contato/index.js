73bd2b5350fa43ec342bc2d48f3f98a2ea8133e8
fatekkl <mathtml.1105@gmail.comm>
2024-02-01 18:54:13 -0300

    alterando variaveis css

--- src/Components/Contato/index.js
+++ src/Components/Contato/index.js
@@ -4,14 +4,6 @@ import './Contato.css'
 const Contato = () => {
     return (
         <section className='container'>
-            <div className='div-links'>
-                <h3>Entre em contato comigo!</h3>
-                <div className='div-copy'>
-                    <div className='copy-item'>
-                        <p>mathtml.1105@gmail.com</p>
-                    </div>
-                </div>
-            </div>
         </section>
     )
 }
