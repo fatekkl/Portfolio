e647dba7dad16cd346d30de44043256959ba7e0e
fatekkl <mathtml.1105@gmail.comm>
2024-03-02 17:33:37 -0300

    adicionado imagem pré hover dos projetos, pagina de contato nao responsiva e atualizando comentarios CSS

--- src/Components/Contato/index.js
+++ src/Components/Contato/index.js
@@ -4,7 +4,25 @@ import './Contato.css'
 const Contato = () => {
     return (
         <section className='container-contato'>
-            
+            <h1>Contato</h1>
+            <div className='container-div-img'>
+                <img alt='email' src='./assets/Contato.png' />
+            </div>
+            <div className='container-copys'>
+                <h2>Fale comigo clicando abaixo!</h2>
+                <div className='clickcopy'>
+                    <div className='icon'>
+                        <img alt='email' src='./assets/email.png'/>
+                    </div>
+                    <span>mathtml.1105@gmail.com</span>
+                </div>
+                <div className='clickcopy'>
+                    <div className='icon'>
+                        <img alt='email' src='./assets/email.png'/>
+                    </div>
+                    <span>mathtml.1105@gmail.com</span>
+                </div>
+            </div>
         </section>
     )
 }
--- src/Components/ProjetoIndividual/index.js
+++ src/Components/ProjetoIndividual/index.js
@@ -3,22 +3,25 @@ import "./ProjetoIndividual.css"
 
 const ProjetoIndividual = (props) => {
 
-
-
     return (
         <div className='project'>
-            <h1>{props.title}</h1>
-            <p>{props.desc}</p>
-            <div className='mini-container'>
-                {props.techs.map((tech , index) => {
-                    return <img key={index} alt='badge' src={tech} />
-                })}
+            <div className="imagem-project">
+                <img alt="Ilustration"src="./assets/project-ligth-img.png"/>
             </div>
-            <div className='button-container'>
-                <a target='_blank' rel='noopener noreferrer' href={props.link} className='preview-button'><img alt='linksymbol' src='./assets/linklogo.png' />Preview</a>
-                
-                <a target='_blank' rel='noopener noreferrer' href={props.repo} className='repo-button'><img id='gitimg' alt='github' src='./assets/github.png' />Code</a>
+            <div className="project-content">
+                <h1>{props.title}</h1>
+                <p>{props.desc}</p>
+                <div className='mini-container'>
+                    {props.techs.map((tech, index) => {
+                        return <img key={index} alt='badge' src={tech} />
+                    })}
+                </div>
+                <div className='button-container'>
+                    <a target='_blank' rel='noopener noreferrer' href={props.link} className='preview-button'><img alt='linksymbol' src='./assets/linklogo.png' />Preview</a>
+                    <a target='_blank' rel='noopener noreferrer' href={props.repo} className='repo-button'><img id='gitimg' alt='github' src='./assets/github.png' />Code</a>
+                </div>
             </div>
+
         </div>
     )
 }
