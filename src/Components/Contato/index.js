d93ef29d0cd27ee0100ba5c68667e8998b63281f
fatekkl <mathtml.1105@gmail.comm>
2024-03-06 09:01:45 -0300

    copy paste da area de contato + responsividade

--- src/Components/Contato/index.js
+++ src/Components/Contato/index.js
@@ -2,25 +2,39 @@ import './Contato.css'
 
 
 const Contato = () => {
+
+    function copy (event)  {
+        const target = event.target.textContent
+        const prevTarget = event.target.textContent
+        navigator.clipboard.writeText(target)
+        event.target.textContent = "COPIADO COM SUCESSO!"
+        event.target.style.color = "#1AFB47"
+        setTimeout(() => {
+            event.target.textContent = prevTarget
+            event.target.style.color = "#fafafa"
+        }, 400)
+
+    }
+
     return (
         <section className='container-contato'>
             <h1>Contato</h1>
             <div className='container-div-img'>
-                <img alt='email' src='./assets/Contato.png' />
+                <img alt='envelope' src='./assets/Contato.png' />
             </div>
             <div className='container-copys'>
-                <h2>Fale comigo clicando abaixo!</h2>
+                <h2>Fale comigo clicando no texto abaixo!</h2>
                 <div className='clickcopy'>
                     <div className='icon'>
-                        <img alt='email' src='./assets/email.png'/>
+                        <img alt='email' src='./assets/envelope.png' />
                     </div>
-                    <span>mathtml.1105@gmail.com</span>
+                    <span onClick={copy}>mathtml.1105@gmail.com</span>
                 </div>
                 <div className='clickcopy'>
                     <div className='icon'>
-                        <img alt='email' src='./assets/email.png'/>
+                        <img alt='envelope' src='./assets/envelope.png' />
                     </div>
-                    <span>mathtml.1105@gmail.com</span>
+                    <span onClick={copy}>mathtml.1105@gmail.com</span>
                 </div>
             </div>
         </section>
