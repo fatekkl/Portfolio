3dd0f7cb2b8d72b261923a7bfdc53dc57d88142a
fatekkl <mathtml.1105@gmail.comm>
2024-03-14 11:25:39 -0300

    fix footer socials responsivity

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -5,7 +5,14 @@ const Footer = () => {
     return (
         <>
             <footer>
-                
+                <div className="div-socials">
+                    <h1>Social</h1>
+                    <div className="div-social-img">
+                        <a target="_blank" rel='noopener noreferrer' href="https://www.linkedin.com/in/fatekkl/" ><img alt="linkedin" src="/assets/linkedin.png" /><p>Linkedin</p></a>
+                        <a target="_blank" rel='noopener noreferrer' href="https://github.com/fatekkl" ><img alt="github" src="/assets/github.png" /><p>Github</p></a>
+                        <a target="_blank" rel='noopener noreferrer' href="https://www.instagram.com/matheuskdask"><img alt="instagram" src="/assets/instagram.png" /><p>Instagram</p></a>
+                    </div>
+                </div>
             </footer>
         </>
     )
