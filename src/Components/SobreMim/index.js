bf675c945fd7b1fedc47a7a0ff07c0eb3f21fd5f
fatekkl <mathtml.1105@gmail.comm>
2024-03-14 17:00:37 -0300

    att sobre-mim texto e adicionando nova parte

--- src/Components/SobreMim/index.js
+++ src/Components/SobreMim/index.js
@@ -5,7 +5,10 @@ const SobreMim = () => {
         <section id='section-sobre' className="Secao2">
             <div className="SobreMim">
                 <h1>Mais sobre mim</h1>
-                <p>Meu nome é Matheus, tenho 17 anos e nasci em Barueri, porém sempre morei em São Paulo. Desde sempre, sempre fui apaixonado por tecnologia, com interesse constante em celulares, computadores, e assim por diante. Inicialmente, criei interesse pelo desenvolvimento de jogos, porém mudei de ideia quando comecei a lidar com o Desenvolvimento Web, pelo qual criei maior curiosidade. Meu primeiro contato com a programação foi por volta dos 10 a 11 anos. Mesmo sem saber o que era, eu criava arquivos .bat no PC e os enviava para amigos, ou até mesmo alterava o HTML de algum site com o "Inspecionar Elemento" e mostrava para as pessoas. Entretanto, no fim de 2022, comecei a estudar programação de forma séria, com conteúdo gratuito na internet. Após isso, consegui acesso aos cursos da Alura, os quais até hoje utilizo. Aprendi stacks como React.js, HTML/CSS, JavaScript e Node.js. Recentemente, tenho focado meus estudos em React e Node, já que são tecnologias que utilizam a mesma linguagem e são importantes atualmente no mercado.
+                <p>Meu nome é Matheus, tenho 17 anos e comecei meus estudos em programação no início de 2022. Inicialmente, foquei na lógica e, em certo ponto, estudei o básico de Front-End. Assim que senti progresso suficiente, aprendi sobre React.js e Node.js. Decidi aprender Node para criar aplicações completas sem depender de outra pessoa. Atualmente, continuo estudando Node e me aprofundando em conceitos da computação. Pretendo continuar aprendendo e explorar outras linguagens e frameworks.
+                </p>
+                <h2>O que estou estudando?</h2>
+                <p> Atualmente, continuo estudando Node e me aprofundando em conceitos da computação. Pretendo continuar aprendendo e explorar outras linguagens e frameworks.
                 </p>
             </div>
             <div className='sobre-img'>
