d0f5e4410c365e4096568628f0aed7afc5948421
fatekkl <mathtml.1105@gmail.comm>
2024-03-15 12:22:09 -0300

    adding text to footer

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -3,18 +3,19 @@ import "./Footer.css"
 
 const Footer = () => {
     return (
-        <>
-            <footer>
-                <div className="div-socials">
-                    <h1>Social</h1>
-                    <div className="div-social-img">
-                        <a target="_blank" rel='noopener noreferrer' href="https://www.linkedin.com/in/fatekkl/" ><img alt="linkedin" src="/assets/linkedin.png" /><p>Linkedin</p></a>
-                        <a target="_blank" rel='noopener noreferrer' href="https://github.com/fatekkl" ><img alt="github" src="/assets/github.png" /><p>Github</p></a>
-                        <a target="_blank" rel='noopener noreferrer' href="https://www.instagram.com/matheuskdask"><img alt="instagram" src="/assets/instagram.png" /><p>Instagram</p></a>
-                    </div>
+        <footer>
+            <div className="container-h1">
+                <h1>2024 © Matheus Oliveira. Todos os direitos reservados.</h1>
+            </div>
+            <div className="div-socials">
+                <h1>Social</h1>
+                <div className="div-social-img">
+                    <a target="_blank" rel='noopener noreferrer' href="https://www.linkedin.com/in/fatekkl/" ><img alt="linkedin" src="/assets/linkedin.png" /><p>Linkedin</p></a>
+                    <a target="_blank" rel='noopener noreferrer' href="https://github.com/fatekkl" ><img alt="github" src="/assets/github.png" /><p>Github</p></a>
+                    <a target="_blank" rel='noopener noreferrer' href="https://www.instagram.com/matheuskdask"><img alt="instagram" src="/assets/instagram.png" /><p>Instagram</p></a>
                 </div>
-            </footer>
-        </>
+            </div>
+        </footer>
     )
 }
 
