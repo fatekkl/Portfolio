271f9511b0ab073f1918b7af7305697d5a8bc72e
fatekkl <mathtml.1105@gmail.comm>
2024-03-15 11:53:31 -0300

    responsivity adjust

--- src/Components/SobreMim/index.js
+++ src/Components/SobreMim/index.js
@@ -5,10 +5,10 @@ const SobreMim = () => {
         <section id='section-sobre' className="Secao2">
             <div className="SobreMim">
                 <h1>Mais sobre mim</h1>
-                <p>Meu nome é Matheus, tenho 17 anos e comecei meus estudos em programação no início de 2022. Inicialmente, foquei na lógica e, em certo ponto, estudei o básico de Front-End. Assim que senti progresso suficiente, aprendi sobre React.js e Node.js. Decidi aprender Node para criar aplicações completas sem depender de outra pessoa. Atualmente, continuo estudando Node e me aprofundando em conceitos da computação. Pretendo continuar aprendendo e explorar outras linguagens e frameworks.
+                <p>Meu nome é Matheus, tenho 17 anos e comecei meus estudos em programação no início de 2022. Inicialmente, foquei na lógica e, em certo ponto, estudei o básico de Front-End. Assim que senti progresso suficiente, aprendi sobre React.js e Node.js. Decidi aprender Node para criar aplicações completas sem depender de outra pessoa.
                 </p>
                 <h2>O que estou estudando?</h2>
-                <p> Atualmente, continuo estudando Node e me aprofundando em conceitos da computação. Pretendo continuar aprendendo e explorar outras linguagens e frameworks.
+                <p>Atualmente, continuo estudando Node e me aprofundando em conceitos da computação. Pretendo continuar aprendendo e explorar outras linguagens e frameworks, tenho grande interesse em linguagens como C#, .Net e Python.
                 </p>
             </div>
             <div className='sobre-img'>
