2523a5398beee54a9d0ef06172f2be826bb4133f
fatekkl <mathtml.1105@gmail.comm>
2024-03-19 16:43:28 -0300

    muda texto sobre mim

--- src/Components/SobreMim/index.js
+++ src/Components/SobreMim/index.js
@@ -8,7 +8,7 @@ const SobreMim = () => {
                 <p>Meu nome é Matheus, tenho 17 anos e comecei meus estudos em programação no início de 2022. Inicialmente, foquei na lógica e, em certo ponto, estudei o básico de Front-End. Assim que senti progresso suficiente, aprendi sobre React.js e Node.js. Decidi aprender Node para criar aplicações completas sem depender de outra pessoa.
                 </p>
                 <h2>O que estou estudando?</h2>
-                <p>Atualmente, continuo estudando Node e me aprofundando em conceitos da computação. Pretendo continuar aprendendo e explorar outras linguagens e frameworks, tenho grande interesse em linguagens como C#, .Net e Python.
+                <p>Atualmente, continuo estudando Node e me aprofundando em conceitos da computação. Pretendo continuar aprendendo e explorar outras linguagens e frameworks, tenho grande interesse em linguagens como C#, Kotlin e Python.
                 </p>
             </div>
             <div className='sobre-img'>
