2424f1fc00ecd9cdcc08a4690f45f3417d587f0b
fatekkl <mathtml.1105@gmail.com>
2024-04-29 12:15:19 -0300

    add: new project

--- src/Components/Projetos/index.js
+++ src/Components/Projetos/index.js
@@ -12,15 +12,19 @@ function Projetos() {
         git: "./assets/gitbadge.png",
         css: "./assets/cssbadge.png",
         html: "./assets/htmlbadge.png",
-        php: "./assets/phpbadge.png"
+        php: "./assets/phpbadge.png",
     }
     return (
         <section id='section-projetos' className='container-section'>
             <div className='background-box'><span>Conheça <strong>meus</strong> projetos<strong>.</strong></span></div>
             <div className='container-projects'>
+
                 <ProjetoIndividual title="CRUD" desc="CRUD é uma sigla para Create, Read, Update e Delete. Foi um projeto que desenvolvi com o intuito de aprender mais sobre APIs, Protocolo HTTP, Assincronicidade e os conceitos básicos de orientação a objetos."techs={[paths.react, paths.git, paths.node, paths.js]} link="" repo="https://github.com/fatekkl/TodoList" />
-                <ProjetoIndividual title="E-Commerce" desc="M&D Closet, foi um freelance que fiz para uma loja de roupas local, com isso tive que interagir com o gateway de pagamento da Asaas e sua API, além disso também interagi com a transportadora a cada pedido que é feito." techs={[paths.html , paths.css , paths.php]} link="https://closetmed.com.br/" repo=""/>
-                <ProjetoIndividual title="Lorem Ipsum" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare enim accumsan, condimentum tortor nec, ultrices eros. Donec sit amet posuere quam. Pellentesque id meturcu" techs={[paths.node , paths.js , paths.git]}/>
+
+                <ProjetoIndividual title="E-Commerce" desc="M&D Closet, foi um freelance que fiz para uma loja de roupas local, com isso tive que interagir com o gateway de pagamento da Asaas e sua API, além disso também interagi com a transportadora a cada pedido que é feito." techs={[paths.html , paths.css , paths.php]} link="https://closetmed.com.br/" repo="https://github.com/fatekkl"/>
+
+                <ProjetoIndividual title="Automação CRM" desc="Nesse projeto freelance, eu e outro desenvolvedor fizemos a automatização dos pedidos e da gestão da empresa, cada pedido feito é enviado para o serviço de gestão com todas as informações sobre o cliente." techs={[paths.node , paths.js]} link="https://ozonsolution.com/"/>
+
                 <ProjetoIndividual title="Lorem Ipsum" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare enim accumsan, condimentum tortor nec, ultrices eros. Donec sit amet posuere quam. Pellentesque id meturcu" techs={[paths.node , paths.js , paths.git]}/>
             </div>
         </section>
