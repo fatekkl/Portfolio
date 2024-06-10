eafc4dcf6f77a631cfcf48ca12e6c16f55e69c07
fatekkl <mathtml.1105@gmail.com>
2024-06-10 18:31:12 -0300

    docs: change text

--- src/Components/SobreMim/index.js
+++ src/Components/SobreMim/index.js
@@ -1,21 +1,33 @@
-import './SobreMim.css'
+import "./SobreMim.css";
 
 const SobreMim = () => {
-    return (
-        <section id='section-sobre' className="Secao2">
-            <div className="SobreMim">
-                <h1>Mais sobre mim</h1>
-                <p>Meu nome é Matheus, tenho 18 anos e comecei meus estudos em programação no início de 2022. Inicialmente, foquei na lógica e, em certo ponto, estudei o básico de Front-End. Assim que senti progresso suficiente, aprendi sobre React.js e Node.js. Decidi aprender Node para criar aplicações completas sem depender de outra pessoa.
-                </p>
-                <h2>O que estou estudando?</h2>
-                <p>Atualmente, continuo estudando Node, me aprofundando em conceitos da computação e principalmente na programação orientada a objetos. Pretendo continuar aprendendo e explorar outras linguagens e frameworks, tenho grande interesse em linguagens como C#, Kotlin e Python.
-                </p>
-            </div>
-            <div className='sobre-img'>
-                <img src='./assets/fotominha.png' alt='Minha Foto' />
-            </div>
-        </section>
-    )
-}
+  return (
+    <section id="section-sobre" className="Secao2">
+      <div className="SobreMim">
+        <h1>Mais sobre mim</h1>
+        <p>
+          Meu nome é Matheus, tenho 18 anos e comecei meus estudos em
+          programação no início de 2022. Quando comecei, meu primeiro contato
+          foi com Python e tenho certo conhecimento sobre a linguagem, porém
+          depois me foquei em JavaScript tanto para FrontEnd quanto para
+          BackEnd.
+        </p>
+        <h2>O que faço atualmente?</h2>
+        <p>
+          Atualmente, tenho me dedicado ao estudo de conceitos da computação e
+          lógica, tais como algoritmos. Além disso, venho aprofundando meus
+          conhecimentos em outras linguagens, como Java, TypeScript e,
+          principalmente, Kotlin. Essa jornada de estudos me levou a explorar
+          também o campo dos bancos de dados, e recentemente tenho me dedicado
+          ao aprendizado de conteúdos relacionados à computação em nuvem,
+          buscando compreender o funcionamento de aplicações em grande escala.
+        </p>
+      </div>
+      <div className="sobre-img">
+        <img src="./assets/fotominha.png" alt="Minha Foto" />
+      </div>
+    </section>
+  );
+};
 
-export default SobreMim
+export default SobreMim;
