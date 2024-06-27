4abd7c594b4301b6ffce176786b7577ddab4d49d
fatekkl <mathtml.1105@gmail.com>
2024-06-27 15:28:23 -0300

    minor changes

--- src/Components/Projetos/index.js
+++ src/Components/Projetos/index.js
@@ -13,16 +13,17 @@ function Projetos() {
         css: "./assets/cssbadge.png",
         html: "./assets/htmlbadge.png",
         php: "./assets/phpbadge.png",
-        sqlite: "./assets/sqlitebadge.png"
+        sqlite: "./assets/sqlitebadge.png",
+        linux: "./assets/linuxbadge.png"
     }
     return (
         <section id='section-projetos' className='container-section'>
             <div className='background-box'><span>Conheça <strong>meus</strong> projetos<strong>.</strong></span></div>
             <div className='container-projects'>
 
-                <ProjetoIndividual title="CRUD" desc="CRUD é uma sigla para Create, Read, Update e Delete. Foi um projeto que desenvolvi com o intuito de aprender mais sobre APIs, Protocolo HTTP, Assincronicidade e os conceitos básicos de orientação a objetos."techs={[paths.react, paths.git, paths.node, paths.js]} link="" repo="https://github.com/fatekkl/TodoList" />
+                <ProjetoIndividual title="Resend" desc="Nesse projeto, eu desenvolvi um serviço de envio de emails em PHP, utilizei uma VPS(Ubuntu), tem diversas funções como operações CRUD com dominios no Resend, envio de e-mails, agendamento de emails, validação de domínio. "techs={[paths.php, paths.git, paths.linux]} link="" repo="https://github.com/fatekkl/TodoList" />
 
-                <ProjetoIndividual title="E-Commerce" desc="M&D Closet, foi um freelance que fiz para uma loja de roupas local, com isso tive que interagir com o gateway de pagamento da Asaas e sua API, além disso também interagi com a transportadora a cada pedido que é feito." techs={[paths.html , paths.css , paths.php]} link="https://closetmed.com.br/" repo="https://github.com/fatekkl"/>
+                <ProjetoIndividual title="E-Commerce" desc="Nesse projeto, foi um freelance que fiz para uma loja de roupas local, com isso tive que interagir com o gateway de pagamento da Asaas e sua API, além disso também interagi com a transportadora a cada pedido que é feito. Segui a interface requisitada pela cliente." techs={[paths.html , paths.css , paths.php]} link="https://closetmed.com.br/" repo="https://github.com/fatekkl"/>
 
                 <ProjetoIndividual title="Automação CRM" desc="Nesse projeto freelance, eu e outro desenvolvedor fizemos a automatização dos pedidos e da gestão da empresa, cada pedido feito é enviado para o serviço de gestão com todas as informações sobre o cliente." techs={[paths.node , paths.js]} link="https://ozonsolution.com/"/>
 
