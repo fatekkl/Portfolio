03f64e0c4e6ed869d25376492ca43a8e83ebbdef
fatekkl <mathtml.1105@gmail.com>
2024-07-29 16:51:15 -0300

    update UI/Responsivity

--- src/Components/ProjetoIndividual/index.js
+++ src/Components/ProjetoIndividual/index.js
@@ -3,25 +3,26 @@ import "./ProjetoIndividual.css"
 
 const ProjetoIndividual = (props) => {
 
+    if (props.preview === 'false'){
+        const button = document.querySelector('.repo-button')
+
+        button.style = 'display: none'
+    }
+
     return (
         <div className='project'>
-            <div className="imagem-project">
-                <img id="project-img" alt="Ilustration"src="./assets/project-ligth-img.png"/>
-            </div>
-            <div className="project-content">
-                <h1>{props.title}</h1>
+            <h1>{props.title}</h1>
+            <div>
                 <p>{props.desc}</p>
                 <div className='mini-container'>
                     {props.techs.map((tech, index) => {
                         return <img key={index} alt='badge' src={tech} />
                     })}
                 </div>
-                <div className='button-container'>
-                    <a target='_blank' rel='noopener noreferrer' href={props.link} className='preview-button'><img alt='linksymbol' src='./assets/linklogo.png' />Preview</a>
-                    <a target='_blank' rel='noopener noreferrer' href={props.repo} className='repo-button'><img id='gitimg' alt='github' src='./assets/github.png' />Code</a>
-                </div>
             </div>
-
+            <div className='button-container'>
+                <a target='_blank' rel='noopener noreferrer' href={props.repo} className='repo-button'><img id='gitimg' alt='github' src='./assets/github.png' />Link</a>
+            </div>
         </div>
     )
 }
--- src/Components/Projetos/index.js
+++ src/Components/Projetos/index.js
@@ -21,13 +21,13 @@ function Projetos() {
             <div className='background-box'><span>Conheça <strong>meus</strong> projetos<strong>.</strong></span></div>
             <div className='container-projects'>
 
-                <ProjetoIndividual title="Resend" desc="Nesse projeto, eu desenvolvi um serviço de envio de emails em PHP, utilizei uma VPS(Ubuntu), tem diversas funções como operações CRUD com dominios no Resend, envio de e-mails, agendamento de emails, validação de domínio. "techs={[paths.php, paths.git, paths.linux]} link="" repo="https://github.com/fatekkl/TodoList" />
+                <ProjetoIndividual title="Resend" desc="Nesse projeto freelance, eu desenvolvi um serviço de envio de emails em PHP, utilizei uma VPS(Ubuntu), tem diversas funções como operações CRUD com dominios no Resend, envio de e-mails, agendamento de emails, validação de domínio. "techs={[paths.php, paths.git, paths.linux]} repo="https://github.com/fatekkl/Resend" />
 
-                <ProjetoIndividual title="E-Commerce" desc="Nesse projeto, foi um freelance que fiz para uma loja de roupas local, com isso tive que interagir com o gateway de pagamento da Asaas e sua API, além disso também interagi com a transportadora a cada pedido que é feito. Segui a interface requisitada pela cliente." techs={[paths.html , paths.css , paths.php]} link="https://closetmed.com.br/" repo="https://github.com/fatekkl"/>
+                <ProjetoIndividual title="E-Commerce" desc="Nesse projeto, eu desenvolvi um e-commerce do zero utilizando Woocommerce. Fui responsável por criar a interface seguindo o Figma, conectar o gateway de pagamento, API da transportadora e fazer o deploy em uma hospedagem da Hostinger. " techs={[paths.html , paths.css , paths.php]} repo="https://closetmed.com.br/"/>
 
-                <ProjetoIndividual title="Automação CRM" desc="Nesse projeto freelance, eu e outro desenvolvedor fizemos a automatização dos pedidos e da gestão da empresa, cada pedido feito é enviado para o serviço de gestão com todas as informações sobre o cliente." techs={[paths.node , paths.js]} link="https://ozonsolution.com/"/>
+                <ProjetoIndividual title="Automação CRM" desc="Nesse projeto freelance, eu e outro desenvolvedor fizemos a automatização dos pedidos e da gestão da empresa, hospedamos o script em um AppScript que observava as requisições feitas para API, capturava os dados e adicionava no sistema de leads com outra requisição POST" techs={[paths.node , paths.js]} repo="https://ozonsolution.com/"/>
 
-                <ProjetoIndividual title="Node com Sequelize" desc="Nesse projeto pessoal, eu desenvolvi um sistema de classes baseado em uma escola e cursos, Utilizei Node e a biblioteca Sequelize para conectar com uma tabela sqlite, enviando, atualizando, deletando e lendo os dados." techs={[paths.node , paths.sqlite , paths.git]} link="https://github.com/fatekkl/NodeSequelize" repo="https://github.com/fatekkl/NodeSequelize"/>
+                <ProjetoIndividual title="Monitoramento Whatsapp" desc="Nesse freelance, eu desenvolvi uma pagina web e consumi a API do Whatsapp para, autenticar usuários gerando um QRCODE e criando instâncias na Evolution, fiz um sistema de seleção de grupos do usuário autenticado e após selecionados, são enviados para o webhook que os monitora." techs={[paths.js, paths.git, paths.html, paths.css]} repo=""/>
             </div>
         </section>
 
